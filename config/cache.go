ee5105d67685622ff1a8b6d5b3500666def2efdf
daddydemir <42716195+daddydemir@users.noreply.github.com>
2024-01-28 02:01:23 +0300

    update: Convert Redis client to a singleton instance.

--- config/cache.go
+++ config/cache.go
@@ -3,15 +3,26 @@ package config
 import (
 	"github.com/redis/go-redis/v9"
 	"strconv"
+	"sync"
 )
 
+var once sync.Once
 var RDB *redis.Client
 
 func RedisClient() {
-	db, _ := strconv.Atoi(Get("REDIS_DB"))
-	RDB = redis.NewClient(&redis.Options{
-		Addr:     Get("REDIS_ADDR"),
-		Password: Get("REDIS_PASS"),
-		DB:       db,
+	RDB = getInstance()
+}
+
+func getInstance() *redis.Client {
+
+	once.Do(func() {
+		db, _ := strconv.Atoi(Get("REDIS_DB"))
+		RDB = redis.NewClient(&redis.Options{
+			Addr:     Get("REDIS_ADDR"),
+			Password: Get("REDIS_PASS"),
+			DB:       db,
+		})
 	})
+
+	return RDB
 }
